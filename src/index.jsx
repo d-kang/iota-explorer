40da6e1b23ee54a9c63ae4e3adfaa303d3d2174c
d-kang <david.kang714@gmail.com>
2018-01-01 11:24:31 -0800

    Require Style file in Index to Apply CSS

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '../assets/style.scss';
 
-ReactDOM.render(<div>hello world</div>, document.getElementById('root'));
+
+ReactDOM.render(<div className='test' id='hi'>hello world!</div>, document.getElementById('root'));
