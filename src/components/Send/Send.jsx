99f752e83041ce700d70cf3f816fde40db7d6df9
d-kang <david.kang714@gmail.com>
2018-01-02 09:06:24 -0800

    Setup Refs to Get Form Data and Add Proof of Work Popup if Submit is Clicked

--- src/components/Send/Send.jsx
+++ src/components/Send/Send.jsx
@@ -11,10 +11,27 @@ import {
   Title,
   Form,
   Button,
+  Collapse,
+  Well,
 } from 'react-bootstrap';
 class Send extends Component {
   state = {
     isActive: false,
+    POW: false,
+  }
+
+  getRefs = () => {
+    this.proofOfWork();
+    const myInputs = {
+      name: this._name.value,
+      value: this._value.value,
+      address: this._address.value,
+      message: this._message.value,
+    };
+    console.log('myInputs', myInputs);
+  }
+  proofOfWork = () => {
+    this.setState({ POW: true });
   }
 
   toggleSidebar = () => {
@@ -39,73 +56,98 @@ class Send extends Component {
               <h1 className="send__header">
                   <p className="send__header-title">Send Message</p>
               </h1>
-                <Row className="show-grid">
-                  <Col xs={12}>
-                    <p className="send__body-balance">
-                      <img className="send__logo" src={logoBlack}/> <strong style={{fontSize: '25px'}}>Balance:</strong>
-                      <Label bsStyle="primary" id="iota__balance">0</Label>
-                    </p>
-                  </Col>
-                  <Col xs={6}>
-                    <Form>
-                      <label htmlFor="name">Your Name</label>
-                      <input type="text" maxLength="16" className="form-control" id="name" placeholder="Enter Name" />
-                    </Form>
-                  </Col>
-                  <Col xs={6}>
-                    <Form>
-                      <label htmlFor="value">Value</label>
-                      <input type="number" className="form-control" id="value" placeholder="Value to Send" />
-                    </Form>
-                  </Col>
-                  <Col xs={12}>
-                    <Form>
-                      <label htmlFor="address">Recipient Address</label>
-                      <input type="text" className="form-control" id="address" placeholder="IOTA address of recipient" />
-                    </Form>
-                  </Col>
-                  <Col xs={12}>
-                    <Form>
-                      <label htmlFor="message">Text Area</label>
-                      <textarea id="message" className="form-control" rows="3" placeholder="Message to send"></textarea>
-                    </Form>
-                  </Col>
-                  <div className="send__button">
-                    <Button id="submit" bsStyle="success" bsSize="large">Submit</Button>
-                  </div>
-
-
-                  {/* <!-- Message to be displayed once successfully sent --> */}
-                  <div id="send__success"></div>
-                  {/* <!-- --> */}
-
-                {/* <!-- Loading screen while doing PoW --> */}
-                <div id="send__waiting">
-                  <div className="row">
-                    <ul className="bokeh">
-                      {/* <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li> */}
-                    </ul>
-                  </div>
-                  <div className="send__waiting-title">
-                    <p>Doing Proof of Work. Please be patient!</p>
-                  </div>
-                  <div className="alert alert-success" role="alert">
-                    <p>While waiting, you can read more about the <a href="https://dev.iota.org" className="alert-link">IOTA API here.</a> Or you can read our Blog on <a href="https://medium.com/iotatangle" className="alert-link">Medium</a>. Always useful to know this stuff.</p>
-                  </div>
+              <Row className="show-grid">
+                <Col xs={12}>
+                  <p className="send__body-balance">
+                    <img className="send__logo" src={logoBlack}/> <strong style={{fontSize: '25px'}}>Balance:</strong>
+                    <Label bsStyle="primary" id="iota__balance">0</Label>
+                  </p>
+                </Col>
+                <Col xs={6}>
+                  <Form>
+                    <label htmlFor="name">Your Name</label>
+                    <input
+                      type="text"
+                      maxLength="16"
+                      className="form-control"
+                      id="name"
+                      ref={name => this._name = name}
+                      placeholder="Enter Name" />
+                  </Form>
+                </Col>
+                <Col xs={6}>
+                  <Form>
+                    <label htmlFor="value">Value</label>
+                    <input
+                      type="number"
+                      className="form-control"
+                      id="value"
+                      ref={value => this._value = value}
+                      placeholder="Value to Send" />
+                  </Form>
+                </Col>
+                <Col xs={12}>
+                  <Form>
+                    <label htmlFor="address">Recipient Address</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="address"
+                      ref={address => this._address = address}
+                      placeholder="IOTA address of recipient" />
+                  </Form>
+                </Col>
+                <Col xs={12}>
+                  <Form>
+                    <label htmlFor="message">Text Area</label>
+                    <textarea
+                      id="message"
+                      ref={message => this._message = message}
+                      className="form-control"
+                      rows="3"
+                      placeholder="Message to send">
+                    </textarea>
+                  </Form>
+                </Col>
+                <div className="send__button">
+                  <Button onClick={this.getRefs} id="submit" bsStyle="success" bsSize="large">Submit</Button>
                 </div>
+
+                <Collapse in={this.state.POW}>
+                  <Well id="enterSeed">
+                    {/* <!-- Message to be displayed once successfully sent --> */}
+                    <div id="send__success"></div>
+                    {/* <!-- --> */}
+
+                    {/* <!-- Loading screen while doing PoW --> */}
+                    <div id="send__waiting">
+                      <div className="row">
+                        <ul className="bokeh">
+                          {/* <li></li>
+                          <li></li>
+                          <li></li>
+                          <li></li> */}
+                        </ul>
+                      </div>
+                      <div className="send__waiting-title">
+                        <p>Doing Proof of Work. Please be patient!</p>
+                      </div>
+                      <div className="alert alert-success" role="alert">
+                        <p>While waiting, you can read more about the <a href="https://dev.iota.org" className="alert-link">IOTA API here.</a> Or you can read our Blog on <a href="https://medium.com/iotatangle" className="alert-link">Medium</a>. Always useful to know this stuff.</p>
+                      </div>
+                    </div>
+                  </Well>
+                </Collapse>
+
+
+
+
               </Row>
             </div>
           </Grid>
-
-
         </main>
-      <Sidebar
-        myClass={this.myClassObj}
-      />
-    </div>
+        <Sidebar myClass={this.myClassObj}/>
+      </div>
     )
   }
 
